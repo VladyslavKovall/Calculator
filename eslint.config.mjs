362ed3466b1d1b089072cc6e7207e971f3530366
VladyslavKovall <fff523179@gmail.com>
2025-09-07 11:02:35 +0300

    calculator

---
+++ eslint.config.mjs
@@ -0,0 +1,38 @@
+import js from "@eslint/js";
+import globals from "globals";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    ignores: ["dist/**", "node_modules/**"], 
+    plugins: { js },
+    extends: ["js/recommended"],
+
+    languageOptions: {
+      ecmaVersion: "latest",       
+      sourceType: "module",        
+      globals: globals.browser,    
+    },
+
+    rules: {
+      
+      "eqeqeq": "error",           
+      "curly": "error",            
+      "no-var": "error",          
+      "prefer-const": "warn",       
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }], 
+      "no-multiple-empty-lines": ["warn", { max: 1 }], 
+      "semi": ["error", "always"], 
+      "quotes": ["error", "double"],
+      "indent": ["error", 2],       
+     
+      "no-console": "off",        
+      "no-alert": "warn",          
+
+      "comma-dangle": ["error", "always-multiline"], 
+      "object-curly-spacing": ["error", "always"],  
+      "array-bracket-spacing": ["error", "never"],  
+    },
+  },
+]);
---
+++ main.js
@@ -0,0 +1,68 @@
+const display = document.getElementById("display");
+const buttons = document.querySelectorAll("button");
+
+let currentInput = "";
+let result = "";
+
+const add = (a, b) => a + b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
+const subtraction = (a, b) => a - b;
+function calculate(expression) {
+  const tokens = expression.match(/\d+(\.\d+)?|[+\-*/]/g);
+  const firstElem = tokens[0];
+  if (!tokens) {
+    return 0;
+  } 
+  if (firstElem === "-") {
+    tokens.splice(0, 2, "-" + tokens[1]);
+  }
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i] === "*" || tokens[i] === "/") {
+      const a = parseFloat(tokens[i - 1]);
+      const b = parseFloat(tokens[i + 1]);
+      const res = tokens[i] === "*" ? multiply(a, b) : divide(a, b);
+      tokens.splice(i - 1, 3, res.toString());
+      i--;
+    }
+  }
+ 
+  for (let i = 0; i < tokens.length; i++) {
+    if (tokens[i] === "+" || tokens[i] === "-") {
+      const a = parseFloat(tokens[i - 1]);
+      const b = parseFloat(tokens[i + 1]);
+      const res = tokens[i] === "+" ? add(a, b) : subtraction(a, b);
+      tokens.splice(i - 1, 3, res.toString());
+      i--;
+    }
+  }
+
+  return parseFloat(tokens[0]);
+}
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+    if (value === "C") {
+      currentInput = "";
+      display.textContent = "0";
+    } else if (value === "=") {
+      const firstChar = currentInput[0];
+
+      if (firstChar === "*" || firstChar === "/") {
+        display.textContent = "Error";
+        return;
+      }
+      result = calculate(currentInput);
+      display.textContent = result;
+      currentInput = result.toString();
+    } else if (value === "⌫") {
+      currentInput = currentInput.slice(0, -1);
+      display.textContent = currentInput || "0";
+    } else {
+      if (display.textContent === "0") {display.textContent = "";}
+      currentInput += value;
+      display.textContent += value;
+    }
+  });
+});
